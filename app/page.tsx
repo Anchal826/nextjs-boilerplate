7ec054c054eea61437c078fd179ab706926fc2c2
Anchal826 <77092518+Anchal826@users.noreply.github.com>
2024-03-31 18:32:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,11 @@ const Screen1: NextPage = () => {
             <div className={styles.timeGeo}>
               <div className={styles.time}>1:11</div>
               <div className={styles.mastersStatusBarPartsA}>
-                <div className={styles.sbIconGeo}>􀋒</div>
+                <img
+                    className={styles.sbIconGeo}
+                    alt=""
+                    src="/sbIconGeo.svg"
+                  />
               </div>
             </div>
           </div>
