f06c0c201e77187f980e14619244e5f4c3c7d775
Anchal826 <77092518+Anchal826@users.noreply.github.com>
2024-04-01 12:44:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Your original Next.js configuration goes here
+};
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+  pwa: {
+    dest: 'public'
+    // Add any other PWA configurations here
+  }
+};
