a30e830077a16f3d60a5c079249d55fe998ee743
Anchal826 <77092518+Anchal826@users.noreply.github.com>
2024-04-01 19:55:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from 'next/head';
 import styles from "./index.module.css";
-import Button from "./NotificationButton.js"; 
+import Button from "./Button.tsx"; 
 
 
 const Screen1: NextPage = () => {
